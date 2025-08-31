dd01871e92fae4a9ca3bec9fed44927873707ecf
David Cemin <davidcemin@gmail.com>
2025-08-30 23:19:30 -0700

    fronimo baseline

---
+++ fronimo/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+    while (1) {
+        printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+        k_sleep(K_SECONDS(1));
+    }
+
+	return 0;
+}
